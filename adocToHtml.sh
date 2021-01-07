09d8a9392c4bc1b5b26e64ae3df09c4fe3cdf4db
Yoshi K <kai.outsider.777+git@gmail.com>
2021-01-07 11:05:00 +0900

    Add php

--- adocToHtml.sh
+++ adocToHtml.sh
@@ -2,3 +2,4 @@
 asciidoctor -D docs -o index.html ./src/main.adoc
 asciidoctor -D docs -o ubuntu.html ./src/ubuntu/main.adoc
 asciidoctor -D docs -o python.html ./src/python/main.adoc
+asciidoctor -D docs -o php.html ./src/php/main.adoc
